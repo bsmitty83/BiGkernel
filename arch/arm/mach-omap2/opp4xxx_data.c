6b5ed11c1ae493b82d5d66b14298f679b5eb5b91
Boy Petersen <boypetersen@gmail.com>
2013-03-23 17:53:41 +0100

    Adjust GPU OC and GPU governor interfaces to function properly with each other
    Conflicts:
    	arch/arm/mach-omap2/omap2plus-cpufreq.c
    	arch/arm/mach-omap2/opp4xxx_data.c
    	include/linux/cpufreq.h

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -376,9 +376,12 @@ static struct omap_opp_def __initdata omap446x_opp_def_list[] = {
 	OPP_INITIALIZER("iva", "dpll_iva_m5x2_ck", "iva", false, 430000000, OMAP4460_VDD_IVA_OPPNITRO_UV),
 	/* IVA OPP5 - OPP-Nitro SpeedBin*/
 	OPP_INITIALIZER("iva", "dpll_iva_m5x2_ck", "iva", false, 500000000, OMAP4460_VDD_IVA_OPPNITRO_UV),
-
 	/* SGX OPP1 - OPP50 */
 	OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", true, 153600000, OMAP4460_VDD_CORE_OPP50_UV),
+#ifdef CONFIG_PVR_GOVERNOR
+        /* SGX OPP2 - OPP100 */
+        OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", true, 307200000, OMAP4460_VDD_CORE_OPP100_UV),
+#else 
 	/* SGX OPP2 - OPP100 */
 	OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", true, 307200000, OMAP4460_VDD_CORE_OPP100_UV),
 	/* SGX OPP3 - OPPOV */
@@ -470,3 +473,4 @@ int __init omap4_opp_init(void)
 	return r;
 }
 device_initcall(omap4_opp_init);
+
