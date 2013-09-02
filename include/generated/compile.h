9edbd8b119cb3615c922e1b55ee959cb8ed1b3e1
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-01 22:01:23 -0400

    fixed previous commits, added configurable -O flags

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 27 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#27 SMP PREEMPT Thu Aug 22 21:40:56 EDT 2013"
+#define LINUX_COMPILE_BY "bsmitty2"
+#define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
+#define LINUX_COMPILER "gcc version 4.8.2 20130624 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.07) "
