286e90a252d5449347547ce69913e46e8b18f9e3
Kostas K <mpokwsths@gmail.com>
2013-09-15 13:16:12 +0300

    Fixed some omap2 warnings

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 43 */
+/* This file is auto generated, version 53 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#43 SMP PREEMPT Tue Sep 17 17:09:54 EDT 2013"
+#define UTS_VERSION "#53 SMP PREEMPT Wed Sep 18 18:33:13 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
