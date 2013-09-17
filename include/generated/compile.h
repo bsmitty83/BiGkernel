188859121b57db8bc10bbe1019cad183425b5c22
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-16 21:14:27 -0400

    Define vm.swappiness according to zram and frontswap (zswap) definition. --mpokwsths

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 37 */
+/* This file is auto generated, version 39 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#37 SMP PREEMPT Mon Sep 16 20:48:31 EDT 2013"
+#define UTS_VERSION "#39 SMP PREEMPT Mon Sep 16 21:11:54 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
