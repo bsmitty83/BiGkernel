1d372ad50465e9d030b30bdfad3d182eabaaa208
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-21 22:07:37 -0400

    OMAP: Change OC frequencies from 1400/1600/1800/2000

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 55 */
+/* This file is auto generated, version 56 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#55 SMP PREEMPT Sat Sep 21 16:50:30 EDT 2013"
+#define UTS_VERSION "#56 SMP PREEMPT Sat Sep 21 21:38:53 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
