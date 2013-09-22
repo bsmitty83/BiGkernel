1f7d1fd61b1ea59b89a3822f8a26d349284001f9
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-22 07:25:42 -0400

    back to old freq for now

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 56 */
+/* This file is auto generated, version 57 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#56 SMP PREEMPT Sat Sep 21 21:38:53 EDT 2013"
+#define UTS_VERSION "#57 SMP PREEMPT Sat Sep 21 23:04:20 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
