63cb5761dbe8c35787bd6d47178a5b82e4536c68
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-23 17:47:05 -0400

    Added SLQB memory allocator.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 60 */
+/* This file is auto generated, version 63 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#60 SMP PREEMPT Sun Sep 22 20:37:31 EDT 2013"
+#define UTS_VERSION "#63 SMP PREEMPT Mon Sep 23 15:34:31 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
