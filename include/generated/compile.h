4b36cb3dfdda71f8cb2f1424d305bef905246bf8
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-30 17:57:07 -0400

    wap subsystem does lazy swap slot free with expecting the page would be swapped out again so we can avoid unnecessary write.
    But the problem in in-memory swap(ex, zram) is that it consumes
    memory space until vm_swap_full(ie, used half of all of swap device)
    condition meet. It could be bad if we use multiple swap device,
    small in-memory swap and big storage swap or in-memory swap alone.
    
    This patch makes swap subsystem free swap slot as soon as swap-read
    is completed and make the swapcache page dirty so the page should
    be written out the swap device to reclaim it.
    It means we never lose it.
    
    I tested this patch with kernel compile workload.
    
    1. before
    
    compile time : 9882.42
    zram max wasted space by fragmentation: 13471881 byte
    memory space consumed by zram: 174227456 byte
    the number of slot free notify: 206684
    
    2. after
    
    compile time : 9653.90
    zram max wasted space by fragmentation: 11805932 byte
    memory space consumed by zram: 154001408 byte
    the number of slot free notify: 426972
    
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Seth Jennings <sjenning@linux.vnet.ibm.com>
    Cc: Nitin Gupta <ngupta@vflare.org>
    Cc: Konrad Rzeszutek Wilk <konrad@darnok.org>
    Cc: Shaohua Li <shli@kernel.org>
    Signed-off-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    
    boype: added necessary swap_info_struct to mm/swapfile.c
    and include/linux/swap.h
    
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 71 */
+/* This file is auto generated, version 72 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#71 SMP PREEMPT Sun Sep 29 22:43:29 EDT 2013"
+#define UTS_VERSION "#72 SMP PREEMPT Mon Sep 30 17:56:03 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
