7edd788698077745a0330ebcc3b481f681010a0b
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-30 19:21:02 -0400

    omap_temp_sensor.c: add module_param to disable thermal throttle - Th…
    …anks FranciscoFranco

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 72 */
+/* This file is auto generated, version 76 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#72 SMP PREEMPT Mon Sep 30 17:56:03 EDT 2013"
+#define UTS_VERSION "#76 SMP PREEMPT Mon Sep 30 19:05:03 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
