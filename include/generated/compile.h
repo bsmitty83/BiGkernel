51aa26cde665c2c1055dc1aec6b02183f08c9bac
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-09-29 22:24:41 -0400

    SR: Custom SR Tuning v1 - supports MPU, IVA, and CORE
    Override kernel default VDDMIN via sysfs (debug fs).
    /sys/kernel/debug/smartreflex/sr_*/vmin
    
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 67 */
+/* This file is auto generated, version 70 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#67 SMP PREEMPT Sun Sep 29 02:02:30 EDT 2013"
+#define UTS_VERSION "#70 SMP PREEMPT Sun Sep 29 22:14:34 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
