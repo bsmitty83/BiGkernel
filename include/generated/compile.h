cd37c7f1d25c552684202fc7a0982e8b1a3281cf
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-10-05 12:48:23 -0400

    Enable CPU-frequency-independent and IVA-frequency-independent GPU vo…
    …ltage scaling
    
    Why would we want increased CORE voltage on certain CPU or IVA frequencies when there's no real need and only wasted battery!?

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 82 */
+/* This file is auto generated, version 83 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#82 SMP PREEMPT Sat Oct 5 12:17:57 EDT 2013"
+#define UTS_VERSION "#83 SMP PREEMPT Sat Oct 5 12:43:12 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
 #define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
