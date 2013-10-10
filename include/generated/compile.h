897921f28b97f44ea178aa8572c0ea3c8e9c3d4d
bsmitty2 <bsmitty2@bsmitty2-HP-Compaq-6710b-RM342UT-ABA.(none)>
2013-10-10 19:10:01 -0400

    cpufreq: interactive: proper touch boost implementation, userspace Po…
    …werHal is disabled during boot.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 90 */
+/* This file is auto generated, version 92 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#90 SMP PREEMPT Thu Oct 10 16:06:15 EDT 2013"
+#define UTS_VERSION "#92 SMP PREEMPT Thu Oct 10 17:26:17 EDT 2013"
 #define LINUX_COMPILE_BY "bsmitty2"
 #define LINUX_COMPILE_HOST "bsmitty2-HP-Compaq-6710b-RM342UT-ABA"
-#define LINUX_COMPILER "gcc version 4.8.2 20130624 (prerelease) (crosstool-NG hg+default-81ead21f86c2 - Linaro GCC 2013.07) "
+#define LINUX_COMPILER "gcc version 4.7.4 20130508 (prerelease) (crosstool-NG hg+default-4c63ce24fccd - Linaro GCC 2013.05) "
